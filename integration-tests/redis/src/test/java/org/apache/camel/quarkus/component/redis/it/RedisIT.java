cdf9663ef1f977007f8451d6fa9938814acff10e
aldettinger <aldettinger@gmail.com>
2023-05-30 17:21:33 +0200

    Attempt to rebase on main May 30

--- integration-tests/redis/src/test/java/org/apache/camel/quarkus/component/redis/it/RedisIT.java
+++ integration-tests/redis/src/test/java/org/apache/camel/quarkus/component/redis/it/RedisIT.java
@@ -16,9 +16,9 @@
  */
 package org.apache.camel.quarkus.component.redis.it;
 
-import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-@NativeImageTest
+@QuarkusIntegrationTest
 class RedisIT extends RedisTest {
 
 }
